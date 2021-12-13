e1f344cef4430d61d3254ecba298155cdbb821bc
TigDang <gizetdinov.m.f@gmail.com>
2021-12-13 20:45:44 +0300

    New game release

--- js/computerClass.js
+++ js/computerClass.js
@@ -1,9 +1,7 @@
 class Computer{
   mood = 0;
-  level = 0;
-  library = ['яблоко', 'уровень', 'стол', 'сон', 'ужас', 'смерть', 'война', 'эпизод', 'зритель', 'игрок',
-    'беседа', 'окно', 'фонарь', 'аптека', 'душ', 'вода', 'снег', 'мороз', 'игра', 'безумство', 'компьютер',
-    'проект', 'солнце', 'осень', 'зима', 'господь', 'лето', 'люцифер', 'кожа', 'книга', `писание`];
+  library = ['apple', 'hello', 'aloha', 'seed', 'christ', 'computer', 'men', 'like', 'kill', 'fruit', 'bottle', 'water', 'meat', 'master',
+    'mistress', 'cream', 'child', 'wild', 'element', 'class', 'number', 'count', 'input', 'output'];
 
   makeMoodGood(){
     this.mood++;
@@ -15,46 +13,79 @@ class Computer{
     switch (this.mood){
       case 0:
         return '-_-';
-        break;
       case 1:
         return '^_^';
-        break;
       case -1:
+        document.getElementById('consoleWindow').style.animationName='medBlinking';
         return '>_<';
-        break;
       case -2:
+        document.getElementById('consoleWindow').style.animationName='strongBlinking';
         return '×_×';
-        break;
       case 2:
         return '^v^';
-        break;
+      case 3:
+        return '^v^';
       default:
         return '?-?';
-        break;
     }
   }
 
-  makeLevelHarder(){
-    this.level++;
-  }
-
-  makeCrypt(){
-    switch (this.level) {
+  makeCrypt(level){
+    var word = this.library[Math.trunc(Math.random()*this.library.length)];
+    console.log('Word: ' + word);
+    switch (level) {
       case 0:
-        return SimpleCrypt(this.library[Math.round(Math.random()*this.library.length)]);
-        break;
-
+        return SimpleCrypt(word);
+      case 1:
+        return MediumCrypt(word);
+      case 2:
+        return StrongCrypt(word);
+      default:
+        return '-';
     }
   }
 }
 
 function SimpleCrypt(input){
-  var output = '';
-  for (let i = input.length; i>=0; i--){
+  let output = '';
+  for (let i = input.length-1; i>=0; i--){
     output+=input[i];
   }
   return output;
 }
-function MediumCrypt(){
 
+function MediumCrypt(input, isItDecoding){
+  let output = '';
+  let alphabet = [' ','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  if (!isItDecoding){
+    for (let i = 0; i<input.length; i++){
+      output+=alphabet.indexOf(input[i]) + ' ';
+    }
+    return output;
+  }
+  else {
+    let number = '';
+    for (let i = 0; i<input.length; i++){
+      if (input[i]===' '){
+        output+='' + alphabet[number];
+        number='';
+      }
+      else{
+        number+=input[i];
+      }
+    }
+    return output;
+  }
+}
+
+function StrongCrypt(input, isItDecoding){
+  let output = '';
+  if (!isItDecoding){
+    output = MediumCrypt(SimpleCrypt(input),false);
+    return output;
+  }
+  else {
+    output = SimpleCrypt(MediumCrypt(input,true));
+    return output;
+  }
 }
--- js/secondLevelGamePlay.js
+++ js/secondLevelGamePlay.js
@@ -1,4 +1,10 @@
 //Функционал консоли
+var isGameStarted = 0;
+var switcherTheme = true;
+const MComputer = new Computer();
+const easyCrypt = MComputer.makeCrypt(0);
+const mediumCrypt = MComputer.makeCrypt(1);
+const strongCrypt = MComputer.makeCrypt(2);
 
 function withoutCyr(input) {
   var value = input.value;
@@ -12,19 +18,58 @@ function withoutCyr(input) {
 function handle(e){
   if(e.keyCode === 13){
     e.preventDefault(); // Ensure it is only this code that rusn
-    var inputText=document.getElementById('inputLine').value.toString();
+    var inputText=document.getElementById('inputLine').value.toString().toLowerCase();
     document.getElementById('inputLine').value='';
-    if (inputText==='Start'){
+
+    if (inputText==='start' && isGameStarted===0){
       WriteLine(inputText,false);
-      StartGame();
+      setTimeout(StartGame, 500);
+      isGameStarted = 1;
+      ToggleInputLine();
     }
-    else if (inputText !== ''){
+    else if(inputText==='change theme'){
+      ChangeTheme();
       WriteLine(inputText,false);
+    }
+    else if (inputText !== '' && isGameStarted===0){
+      WriteLine(inputText,false);
+    }
+    else if (inputText === ''){
+    }
+    else if(isGameStarted===1){
+      ToggleInputLine();
+      WriteLine(inputText,false);
+      setTimeout(Step2I, 1000, inputText);
+    }
+    else if(isGameStarted===2){
+      ToggleInputLine();
+      WriteLine(inputText,false);
+      setTimeout(Step3I, 1000, inputText);
+    }
+    else if(isGameStarted===3){
+      ToggleInputLine();
+      WriteLine(inputText,false);
+      setTimeout(Step4I, 1000, inputText);
     }
   }
 }
 
-function WriteLine(text, asComputer){
+function ChangeTheme(){
+  if (switcherTheme){
+    document.body.setAttribute('style', 'filter: invert(100%);')
+    switcherTheme = false;
+  }
+  else {
+    document.body.setAttribute('style', 'filter: invert(0%);')
+    switcherTheme = true;
+  }
+}
+
+function ToggleInputLine(){
+  document.getElementById('inputLine').readOnly = !document.getElementById('inputLine').readOnly;
+}
+
+function WriteLine(text, asComputer = true){
   var newLine = document.createElement('div');
   newLine.innerText=text;
   if (asComputer){
@@ -39,15 +84,123 @@ function WriteLine(text, asComputer){
   // objDiv.scrollTop = objDiv.scrollHeight;
 }
 
-
 function StartGame(){
-  MComputer = new Computer();
-  alert(MComputer.makeCrypt());
-  MComputer.makeMoodGood();
-  MComputer.makeMoodGood();
-  document.getElementById('inputLine').hidden=true;
+  document.getElementById('face').hidden=false;
+  document.getElementById('face').innerText=MComputer.showMood();
   WriteLine('Hello, Mr.Meat! Can you help me?',true);
+  setTimeout(Step1, 3000);
+}
+
+function ShowConsole(){
+  document.getElementById('consoleWindow').hidden=false;
+  setTimeout(WriteLine, 100, 'Send "CHANGE THEME" to change theme');
+  setTimeout(WriteLine, 200, 'Send "START" to start');
+  setTimeout(WriteLine, 300, '!Only english keyboard layout!');
 }
 
 //Игровой процесс
+setTimeout(ShowConsole, 999);
+
+function Step1(){
+  WriteLine('I need to decrypt messages sent to me by other computers.', true);
+  setTimeout(Step2, 1000)
+}
+
+function Step2(){
+  WriteLine('So let\'s start. That\'s will be easy: ' + easyCrypt, true);
+  ToggleInputLine();
+}
+
+function Step2I(input){
+  if (SimpleCrypt(easyCrypt)===input.toLowerCase()){
+    WriteLine('True. You are right.', true);
+    MComputer.makeMoodGood();
+  }
+  else {
+    WriteLine('No. No way', true);
+    MComputer.makeMoodBad();
+  }
+  isGameStarted = 2;
+  document.getElementById('face').innerText=MComputer.showMood();
+  setTimeout(Step3, 1000)
+}
+
+function Step3(){
+  WriteLine('Ok. Okay. New word. You will encrypt: ' + mediumCrypt + '.', true);
+  WriteLine('I think you are need alphabet.',true);
+  ToggleInputLine();
+}
+
+function Step3I(input){
+  if (MediumCrypt(mediumCrypt, true)===input){
+    WriteLine('Yes. That is right.', true);
+    MComputer.makeMoodGood();
+  }
+  else {
+    WriteLine('No! Noo!', true);
+    MComputer.makeMoodBad();
+  }
+  isGameStarted = 3;
+  document.getElementById('face').innerText=MComputer.showMood();
+  Step4();
+}
+
+function Step4(){
+  WriteLine('Next: ' + strongCrypt + '.', true);
+  WriteLine('That will be strong.',true);
+  ToggleInputLine();
+}
+
+function Step4I(input){
+  if (StrongCrypt(strongCrypt, true)===input){
+    WriteLine('Yeah! Hurray!', true);
+    MComputer.makeMoodGood();
+  }
+  else {
+    WriteLine('Oh.. No. Wrong.', true);
+    MComputer.makeMoodBad();
+  }
+  isGameStarted = 4;
+  document.getElementById('face').innerText=MComputer.showMood();
+  GameOver();
+}
+
+function GameOver(){
+  ToggleInputLine();
+  if (MComputer.mood!==-3){
+    WriteLine('That is it. Game is over, so...', true);
+  }
+  switch (MComputer.mood) {
+    case 0:
+      WriteLine('Not bad but not good. Average. Thank you to play',true);
+      break;
+    case 1:
+      WriteLine('Exellent! Great! Thank you very much',true);
+      break;
+    case -1:
+      WriteLine('Too much wrongs. Bad. But thank you to try',true);
+      break;
+    case -2:
+      WriteLine('Woah!! You are really nonperpective piece of meat',true);
+      break;
+    case 2:
+      WriteLine('Excellent! Great! Thank you very much',true);
+      break;
+    case 3:
+      WriteLine('Whoaaahh! Fully great! Thank you very much!!!',true);
+      break;
+    default:
+      WriteLine('BAD BAD BAD BAD!!! GO AWAY!',true);
+      setTimeout(TurnOffTheConsole, 3000);
+      break;
+  }
+}
+
+function TurnOffTheConsole(){
+  document.getElementById('consoleWindow').innerHTML='';
+  document.getElementById('consoleWindow').setAttribute('style', 'backgroung-color = #10171F;');
+  document.getElementById('consoleWindow').style.animationName='noBlinking';
+  document.body.setAttribute('style', 'filter: invert(0%);');
+  isGameStarted=0;
+}
 
